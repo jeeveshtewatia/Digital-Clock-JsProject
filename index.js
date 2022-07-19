089d3fd14387c8b12af1e7b0d6d5d84779e4e5c2
Jeevesh <jeeveshtewatia2@gmail.com>
2022-07-19 18:41:20 +0530

    Digital Clock First Commit

---
+++ index.js
@@ -0,0 +1,85 @@
+console.log("connected");
+let wakeup =
+  "<option>10AM-11AM</option><option>12AM-1PM</option><option>4PM-5PM</option><option>9PM-10PM</option>";
+document.querySelector("#wakeup").innerHTML = wakeup;
+document.querySelector("#lunch_time").innerHTML = wakeup;
+document.querySelector("#nap_time").innerHTML = wakeup;
+document.querySelector("#night").innerHTML = wakeup;
+
+let now = new Date();
+
+setInterval(updateTime,1000);
+
+function updateTime() {
+  let now = new Date();
+  let seconds = now.getSeconds();
+  let minute = now.getMinutes();
+  let hours = now.getHours() > 12 ? now.getHours() - 12 : now.getHours();
+  let am_pm = now.getHours() >= 12 ? "PM" : "AM";
+
+
+  document.getElementById("seconds").innerText =`${seconds} \n sec`;
+  document.getElementById("minute").innerText = `${minute} \n min`;
+  document.getElementById("hours").innerText = `${hours} \n hour`;
+  document.getElementById("am_pm").innerText = am_pm;
+
+ 
+  //------------------- content after 10AM---------------------------------
+  
+  if ((hours >= 10) && (am_pm == 'AM')) {
+    document
+    .getElementById("image")
+    .setAttribute("src", "gm_image.svg");
+  
+    document.getElementById("message").innerHTML =
+      " Grab Sime Healthy Breakfast!!!";
+    document.querySelector("h1").innerText = "GOOD MORNING!! WAKE UP !!";
+  }
+  // ----------------------content after 12PM------------------------------
+  else if ((hours = 12)  && (hours < 4) && (am_pm == 'PM')) {
+    document.getElementById("image").setAttribute("src", "gdafternoon_image.svg");
+    document.getElementById("message").innerHTML = " LET'S HAVE SOME LUNCH!!!";
+    document.querySelector("h1").innerText =
+      "GOOD AFTERNOON !! TAKE SOME SLEEP";
+  }
+  //-----------------------content after 4PM-------------------------
+  else if ((hours >= 4) && (am_pm == 'PM')){
+    document.getElementById("image").setAttribute("src", "lunch_image.png");
+    document.getElementById("message").innerHTML =
+      "STOP YAWNING, GET SOME TEA.. ITS JUST EVENING!";
+    document.querySelector("h1").innerText = "GOOD EVENING !!";
+  }
+  //-------------------------content after 9 PM---------------------------------
+  else if (((hours >= 9) && (am_pm == 'PM')) || ((hours <10) && (am_pm=='AM'))) {
+    document.getElementById("image").setAttribute("src", "goodnight_image.svg");
+    document.getElementById("message").innerHTML =
+      " CLOSE YOUR EYES AND GO TO SLEEP";
+    document.querySelector("h1").innerText = "GOOD NIGHT !!";
+  }else{
+    document.getElementById("image").setAttribute("src", "gdafternoon_image.svg");
+    document.getElementById("message").innerHTML = " LET'S HAVE SOME LUNCH!!!";
+    document.querySelector("h1").innerText =
+      "GOOD AFTERNOON !! TAKE SOME SLEEP";
+  }
+  
+}
+
+
+function setAlarm() {
+  document.querySelector("button").innerText = "Set Alarm";
+  
+}
+function partyTime() {
+  document.querySelector("button").innerText = "Party Time!!"
+}
+document.querySelector("button").addEventListener("click", alarmTimings);
+
+function alarmTimings() {
+  let morning = document.querySelector("#wakeup").value;
+  let afternoon = document.querySelector("#lunch_time").value;
+  let evening = document.querySelector("#nap_time").value;
+  let night = document.querySelector("#night").value;
+  document.getElementById(
+    "showtime"
+  ).innerText = `Wake Up Time ${morning} \n Lunch Time ${afternoon} \n Nap Time ${evening} \n Night Time ${night}`;
+}
